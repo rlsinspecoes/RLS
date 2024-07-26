a49d0c022c9a49a9ef2b7eec740e0d3571ff2c24
FelipeRVNK <felipervnk@gmail.com>
2024-07-26 12:43:18 -0300

    Add /rls to links

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App(){
         <>
             <Routes>
                 <Route path="/" element={<Layout><Home/></Layout>} />
-                <Route path="/#/serviços" element={<Layout><Servicos></Servicos></Layout>} />
-                <Route path="/#/historia" element={<Layout><Historia/></Layout>} />
-                <Route path="/#/portfolio" element={<Layout><Portfolio/></Layout>} />
-                <Route path="/#/contatos" element={<Layout><Contatos/></Layout>} />
+                <Route path="/RLS/serviços" element={<Layout><Servicos></Servicos></Layout>} />
+                <Route path="/RLS/historia" element={<Layout><Historia/></Layout>} />
+                <Route path="/RLS/portfolio" element={<Layout><Portfolio/></Layout>} />
+                <Route path="/RLS/contatos" element={<Layout><Contatos/></Layout>} />
                 
             </Routes>
         
